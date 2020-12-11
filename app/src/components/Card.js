580f25206b09b1d0edd8b2f66e9c700a50b5b0fb
Samuel Lee <samuelxlee@gmail.com>
2020-12-10 18:17:21 -0800

    fixed issues with image rendering for throne of eldraine set

--- app/src/components/Card.js
+++ app/src/components/Card.js
@@ -5,13 +5,18 @@ const Card = (props) => {
         <>
             <a href={props.card.scryfall_uri} target="_blank" rel="noreferrer">
                 {props.card.card_faces ?
-                    props.card.card_faces.map(card_face => {
-                        return (
-                            <div key={card_face.illustration_id}>
-                                <img src={card_face.image_uris.normal} alt={card_face.name} />
-                                <p>{card_face.name}</p>
-                            </div>)
-                    })
+                    props.card.card_faces.image_uris ?
+                        props.card.card_faces.map(card_face => {
+                            return (
+                                <div key={card_face.illustration_id}>
+                                    <img src={card_face.image_uris.normal} alt={card_face.name} />
+                                    <p>{card_face.name}</p>
+                                </div>)
+                        })
+                        : <div>
+                            <img src={props.card.image_uris.normal} alt={props.card.name} />
+                            <p>{props.card.name}</p>
+                        </div>
                     :
                     <div>
                         <img src={props.card.image_uris.normal} alt={props.card.name} />
